4b44c19bcdd5f3724c0f9db7f3fe8a8408fbde9f
Noor-26 <nk26982698@gmail.com>
2023-02-26 01:14:15 +0600

    showed modal and changed styles

--- src/app/components/BookRoom.tsx
+++ src/app/components/BookRoom.tsx
@@ -3,7 +3,9 @@ import { useEffect, useState } from 'react';
 import moment, { Moment } from 'moment';
 import 'react-calendar/dist/Calendar.css';
 import { Calendar } from 'react-calendar';
+import Modal from 'react-modal';
 import { KeyboardEvent } from 'react';
+import { FiCheckCircle } from 'react-icons/fi';
 
 
 // interfaces 
@@ -39,6 +41,19 @@ interface BookingSlot {
   };
   
   
+  // custom styles for modal
+  const modalStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+  
+ 
  
   
 const BookRoom = () => {
@@ -46,8 +61,10 @@ const BookRoom = () => {
     const [showCalendar, setShowCalendar] = useState(false);
     const [selectedDate, setSelectedDate] = useState<Date>(new Date());
     const [selectedTime, setSelectedTime] = useState<Moment | null>(null);
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
     const [currentTime, setCurrentTime] = useState('');
 
+      //  using useEffect for changing user current time continuouesly 
     useEffect(() => {
       const intervalId = setInterval(() => {
         setCurrentTime(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
@@ -56,6 +73,7 @@ const BookRoom = () => {
       return () => clearInterval(intervalId);
     }, []);
     
+      // function for  handling the duration time   
     const handleDurationChange = (event: React.ChangeEvent<HTMLInputElement> & KeyboardEvent<HTMLInputElement>) => {
         setDuration(event.target.value);
         if (event.key === 'Enter') {
@@ -63,15 +81,29 @@ const BookRoom = () => {
         }
         };
     
+      // function for selecting the calender date 
     const onCalendarSelect = (date: Date) => {
         setSelectedDate(date);
         setSelectedTime(null);
       };
     
+      // function for selecting the time
       const onTimeSelect = (time: Moment) => {
         setSelectedTime(time);
       };
+
+      // function for booking the conference room
+    const onBookNowClick = () => {
+      setIsModalOpen(true);
+    };
   
+      // function for closing the modal
+    const closeModal = () => {
+      setIsModalOpen(false);
+    };
+
+
+  // function for getting the time slots
       const getTimeSlots = (): BookingSlot[] => {
         const slots: BookingSlot[] = [];
         const schedule = bookingData.schedule;
@@ -102,10 +134,11 @@ const BookRoom = () => {
         return slots;
       };
     
+      // calling the function to get time slots here 
       const timeSlots = getTimeSlots();
       
   return (
-    <div className='max-w-7xl mx-auto min-h-[90vh] flex justify-center '>
+    <div className='max-w-7xl mx-auto min-h-[90vh] flex justify-center px-8'>
         <div className='flex flex-col justify-center items-center border my-auto rounded-lg py-4  px-6'>
 
         {/* showing user current time */}
@@ -114,7 +147,7 @@ const BookRoom = () => {
     </div>
     {/* getting meeting duration  */}
     <div className="mb-4 flex justify-center items-center">
-      <label htmlFor="duration" className="block text-gray-700 font-bold mb-2">
+      <label htmlFor="duration" className="block text-gray-700 text-xl  md:text-2xl mb-2">
         Enter Meeting Duration(min) 
       </label>
       <input
@@ -126,10 +159,10 @@ const BookRoom = () => {
       />
     </div>
 
-    { showCalendar ? <><div className="flex justify-evenly">
+    { showCalendar && <><div className="flex justify-evenly flex-col lg:flex-row">
 
     <div className="mb-4">
-      <label htmlFor="date-picker" className="block text-gray-700 font-bold mb-2">
+      <label htmlFor="date-picker" className="block text-gray-700 Abeezee font-bold mb-2">
         Select Date
       </label>
       <Calendar
@@ -139,12 +172,12 @@ const BookRoom = () => {
         className="border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       />
     </div>
-    {selectedDate && timeSlots.length > 0 && (
+    {selectedDate  && (
       <div className="mb-5 ml-3">
-        <label htmlFor="time-picker" className="block text-gray-700 font-bold mb-2">
+        <label htmlFor="time-picker" className="block text-gray-700 Abeezee font-bold mb-2">
           Select Time
         </label>
-       <div className="grid grid-cols-4 gap-4  py-2  max-h-[45vh] overflow-y-auto ">
+       { timeSlots.length > 0 ?<div className="grid grid-cols-3 md:grid-cols-4 gap-4  py-2   md:max-h-[45vh] overflow-y-auto ">
           {timeSlots.map((timeSlot) => (
             <button 
             key={timeSlot.startTime.toISOString()}
@@ -156,14 +189,14 @@ const BookRoom = () => {
               {moment(timeSlot.startTime).format('h:mm A')} 
             </button>
           ))}
-        </div>
+        </div> : <h1 className='capitalize text-xl  text-[#ff8259] text-center'> No schedule on this</h1> }
       </div>
     )}
     </div>
 
     <div className="flex justify-center">
       <button
-       
+         onClick={onBookNowClick}
         disabled={!selectedTime}
         className={`bg-[#ff8259] text-white py-2 px-4 rounded-lg focus:outline-none ${
           !selectedTime ? 'opacity-50 cursor-not-allowed' : ''
@@ -172,9 +205,27 @@ const BookRoom = () => {
         Book Now
       </button>
     </div>
-    </>: <h1 className='capitalize text-xl  text-[#ff8259] text-center'> Please Input the meeting length and click enter to pick the date & time</h1> }
+    </> }
     </div>
+    <Modal isOpen={isModalOpen} onRequestClose={closeModal} style={modalStyles}>
+      <div className='text-center min-w-[60vw]'>
 
+      <h2  className='flex justify-center'><FiCheckCircle className=" text-5xl md:text-7xl font-bold  text-[#15d611] mb-4"/></h2>
+      <h4 className=' text-3xl md:text-5xl Abeezee'>Great!</h4>
+      <p className='  text-lg md:text-xl font-medium'>Your Booked Time</p>
+      <p className='text-lg md:text-xl font-medium'>Date:  {moment(selectedDate).format('Do MMM YYYY')}, Time:  {moment(selectedTime).format('h:mm A')}</p>
+      <button onClick={() =>{
+        closeModal()
+       setShowCalendar(false)
+       setSelectedDate(new Date());
+       setSelectedTime(null);
+       setDuration("")
+       }} className="bg-[#F25A2C] text-white py-2 px-6 focus:outline-none ml-4 gabriela rounded-full my-2">
+        ok
+        </button>
+      </div>
+
+    </Modal>
 </div>
   )
 }
--- src/app/components/Navber.tsx
+++ src/app/components/Navber.tsx
@@ -2,22 +2,45 @@ import { FaHome } from 'react-icons/fa';
 const Navber = () => {
   return (
     <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl"><FaHome className='mx-2 text-[#F25A2C] '/>Saarte Investeering</a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
-      <li><a className='font-semibold'>Conference</a></li>
-      <li><a>Developments</a></li>
-      <li><a>Blog</a></li>
+       <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+      <li><a className="Abeezee">Home</a></li>
+      <li><a className='font-semibold Abeezee'>Conference</a></li>
+      <li><a className="Abeezee">Developments</a></li>
+      <li><a className="Abeezee">Blog</a></li>
       <li tabIndex={0}>
-        <a>
+        <a className="Abeezee">
           EN
           <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
         </a>
         <ul className="p-2 bg-base-100">
-          <li><a>BN</a></li>
+          <li><a className="Abeezee">BN</a></li>
+        </ul>
+      </li>
+  
+      </ul>
+    </div>
+  <div className="flex flex-nowrap">
+    <a className="btn btn-ghost normal-case text-xl"><FaHome className='mx-2 text-[#F25A2C] text-[20px]'/>Saarte Investeering</a>
+  </div>
+  </div>
+  <div className=" navbar-end hidden lg:flex">
+    <ul className="menu menu-horizontal px-1"> 
+      <li><a className="Abeezee">Home</a></li>
+      <li><a className='font-semibold Abeezee'>Conference</a></li>
+      <li><a className="Abeezee">Developments</a></li>
+      <li><a className="Abeezee">Blog</a></li>
+      <li tabIndex={0}>
+        <a className="Abeezee">
+          EN
+          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+        </a>
+        <ul className="p-2 bg-base-100">
+          <li><a className="Abeezee">BN</a></li>
         </ul>
       </li>
   
